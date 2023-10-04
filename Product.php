6cf3035e8d1a57bd786c6f62d513f437ee7f0b67
Tanvir Azam <119413613+tanvirazam@users.noreply.github.com>
2023-10-04 22:00:16 +0600

    Add files via upload

---
+++ Product.php
@@ -0,0 +1,27 @@
+
+<?php
+class Product{
+    private $id;
+    private $name;
+    private $price;
+
+   
+    public function __construct($id, $name, $price){
+        $this->id = $id;
+        $this->name = $name;
+        $this->price = $price;
+    }
+    public function getFormattedPrice() {
+        return number_format($this->price, 2);
+    }
+    public function showDetails() {
+        echo "Product Details:" . PHP_EOL;
+        echo "- ID: " . $this->id . PHP_EOL;
+        echo "- Name: " . $this->name . PHP_EOL;
+        echo "- Price: $" . $this->getFormattedPrice() . PHP_EOL;
+    }
+
+}
+
+$product = new Product(1, 'T-shirt', 19.99);
+$product->showDetails();
